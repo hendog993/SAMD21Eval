54d4084f697d912c0b9b290b2ab9792d05054479
Henry V. Gilbert <henrygilbert609@gmail.com>
2022-11-18 06:45:16 -0600

    gitignore

--- firmware/SAMD21Eval.X/build/default/production/_ext/1303591613/delay.o.d
+++
@@ -1,4 +0,0 @@
-build/default/production/_ext/1303591613/delay.o: ../src/Generic/delay.c \
- ../src/Generic/delay.h
-
-../src/Generic/delay.h:
--- firmware/SAMD21Eval.X/build/default/production/_ext/1303591613/ws2812b.o.d
+++
@@ -1,2 +0,0 @@
-build/default/production/_ext/1303591613/ws2812b.o: \
- ../src/Generic/ws2812b.c
